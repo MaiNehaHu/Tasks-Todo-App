376f9c07cbc1adb1549a2204e97f35bb1af7b530
MaiNehaHu <neha207y1a0423@mlritm.ac.in>
2023-03-26 06:28:34 +0530

    comments and rearanging code to understand

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,17 @@ const LOCAL_STORAGE_KEY = "tasks"; /**key name*/
 
 const getItems = () => {
   let list =
-    localStorage.getItem(
-      "tasks"
-    ); /**With the helpof keyname we are getting the list */
-  console.log(list);
-  if (list) {
+    localStorage.getItem("tasks"); 
+    /**With the help of keyname we are getting the list */
+  
+    console.log(list);
+  
+    if (list) {
     return JSON.parse(localStorage.getItem("tasks"));
+    /**localStorage.setItems("key", "value") 
+     * JSON.parse bcz Local Storage stores strings only
+     * but we are working with different variables
+    */
   } else {
     return [];
   }
@@ -22,51 +27,57 @@ function App() {
   const todoRef = useRef(); /**to get refernce of input field */
 
   function Random() {
-    return Math.floor(Math.random() * 100001);
+    return Math.floor(Math.random() * Math.pow(10,10));
   }
 
   function handleAdd() {
     const name = todoRef.current.value; /**to get the value in input field */
     if (name === "") return;
 
-    let key = Random();
+    let key = Random(); /**to get random key */
 
     setTasks((prevTodos) => {
       return [...prevTodos, { id: key, name: name, complete: false }];
-      /**key should be unique so it is just name*/
+      /**key should be unique so generating using random*/
       /**This is getting passed as tasks to TodoList */
     });
     console.log(name);
   }
 
-  function handleClearTasks() {
+  function handleClearTasks() { /**handle radio checked tasks*/
     const newTasks = tasks.filter((todo) => !todo.complete);
+    /**updating the list withe the tasks which are not completed*/
     setTasks(newTasks);
   }
+
+   /**Handling radio toggle */
+   function toggleTask(id) {
+    const newTasks = [...tasks]; /**coopying in another array*/
+    const todo = newTasks.find((todo) => todo.id === id);
+
+    todo.complete = !todo.complete;
+    setTasks(newTasks); /**updating list with only not done tasks*/
+  }
+
+  function handleClearAll(){ /**own code */
+  const newTasks = [];   /**updating the list with no tasks */
+  setTasks(newTasks);
+}
 
   /**Local stroage */
 
   /**Everytime anything changes It will get called
    * It will store it into local storage
    * But if we refresh it will get lost */
+   /**That's why we use another some code in the starting of the component code*/
+
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(tasks));
-  }, [tasks]);
+    /**localStorage.setItems("key", "value") 
+     * "value" is stringify bcz Local Storage stores strings only
+    */
+  }, [tasks]); /**From tasks array*/
 
-  /**That's why we use another useEffect above this */
-
-  /**Handling radio toggle */
-  function toggleTask(id) {
-    const newTasks = [...tasks];
-    const todo = newTasks.find((todo) => todo.id === id);
-
-    todo.complete = !todo.complete;
-    setTasks(newTasks);
-  }
-  function handleClearAll(){ /**own code */
-    const newTasks = [];
-    setTasks(newTasks);
-  }
 
   return (
     <div className="container">
